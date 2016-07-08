36b6f556bae20effc06559431f1e72d1c437149a
Abdul Halim <halim.bepari@gmail.com>
2016-07-08 09:54:49 +0100

    Adding sample data

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var data = require("../data.json");
+console.log(data);
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', data: data });
 });
 
 module.exports = router;
