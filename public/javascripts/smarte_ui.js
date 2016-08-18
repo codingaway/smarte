5a4bc72d199577f7c26585a823e6c99dd6a29dee
Abdul Halim <halim.bepari@gmail.com>
2016-08-18 09:34:27 +0100

    Reducing chart size

--- public/javascripts/smarte_ui.js
+++ public/javascripts/smarte_ui.js
@@ -23,15 +23,15 @@ $(document).ready(function () {
     }
 
     var chart,
-            categories = ['Categorie 1', 'Categorie 2', 'Categorie 3', 'Categorie 4', 'Categorie 5', 'Categorie 6', 'Categorie 7', 'Categorie 8', 'Categorie 9', 'Categorie 10', 'Categorie 11', 'Categorie 12', 'Categorie 13', 'Categorie 14', 'Categorie 15', 'Categorie 16', 'Categorie 17', 'Categorie 18', 'Categorie 19', 'Categorie 20', 'Categorie 21', 'Categorie 22', 'Categorie 23', 'Categorie 24', 'Categorie 25', 'Categorie 26', 'Categorie 27', 'Categorie 28', 'Categorie 29', 'Categorie 30'],
-            serie1 = [3, 4, 0, 4, 2, 0, 0, 3, 3, 1, 2, 4, 4, 0, 3, 2, 3, 1, 3, 1, 3, 3, 1, 0, 1, 2, 3, 1, 2, 3],
-            serie2 = [2, 1, 2, 3, 2, 1, 0, 2, 2, 1, 2, 4, 4, 1, 3, 3, 3, 2, 2, 2, 4, 2, 1, 1, 2, 0, 1, 0, 0, 3];
+            categories = ['Categorie 1', 'Categorie 2', 'Categorie 3', 'Categorie 4', 'Categorie 5', 'Categorie 6', 'Categorie 7', 'Categorie 8', 'Categorie 9', 'Categorie 10'],
+            serie1 = [3, 4, 0, 4, 2, 0, 0, 3, 3, 1],
+            serie2 = [2, 1, 2, 3, 2, 1, 0, 2, 2, 1];
     var chartingOptions = {
         chart: {
             renderTo: 'zonechart',
             type: 'column',
             backgroundColor: 'transparent',
-            height: 140,
+            height: 100,
             marginLeft: 3,
             marginRight: 3,
             marginBottom: 0,
